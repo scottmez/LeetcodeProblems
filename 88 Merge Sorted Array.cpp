0dc48300161c990d4b31c7ebd22d13530cc8217e
scottmez <scottmesdjian10@gmail.com>
2024-10-21 10:47:14 -0700

    add merge problem

---
+++ 88 Merge Sorted Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        //increasing or same number
+        //m and n can be different
+        vector<int> nums_comb;
+        int w = m - 1;
+        int v = n - 1;
+        //we fill nums1 backwards since we can save the data from nums1 in itself until we need it
+        for (int i = m + n - 1; i > -1; i--) { // TODO:review this
+            //check which number is bigger, do that first
+            //we also want to use nums1 if v <= -1
+            //except when w <= -1
+            if (w > -1 && (v <= -1 || nums1[w] > nums2[v])) {
+                nums1[i] = nums1[w];
+                w--;
+            }
+            else {
+                nums1[i] = nums2[v];
+                v--;
+            }
+        }
+    }
+};
