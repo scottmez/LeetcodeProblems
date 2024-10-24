59142958c62160c81a6c0d559666d59ee72fd1f1
scottmez <scottmesdjian10@gmail.com>
2024-10-24 13:41:43 -0700

    improved 55

--- 55 Jump Game.cpp
+++ 55 Jump Game.cpp
@@ -5,20 +5,17 @@ public:
     bool canJump(vector<int>& nums) {
         vector<bool> canBe(nums.size(), false); //initialize a vector of bools of length nums.size() where every value is false
         canBe[0] = true; //we start at the first position so we "can be" there
-        queue<int> toDo;
-        toDo.push(0);
-        while(!toDo.empty()) {
-            int index = toDo.front();
-            toDo.pop();
-            int numToJump = nums[index];
-            nums[index] = 0; //this makes sure we wont repeat the check at any index in nums, but assumes we dont need the input array after this function
-            if (index + numToJump >= nums.size() - 1) {
+        for(int i = 0; i < nums.size(); i++) {
+            if (!canBe[i]) {
+                continue;
+            }
+            int numToJump = nums[i];
+            if (i + numToJump >= nums.size() - 1) {
                 return true;
             }
-            for (int i = 1; i <= numToJump; i++) {
-                int nextIndex = i + index;
+            for (int j = 0; j <= numToJump; j++) {
+                int nextIndex = j + i;
                 canBe[nextIndex] = true;
-                toDo.push(nextIndex);
             }
         }
         return canBe[nums.size()-1];
