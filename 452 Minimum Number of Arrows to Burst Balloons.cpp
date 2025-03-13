0ecbbd22d9f8311c846b26b04e68635734cfb8e3
scottmez <scottmesdjian10@gmail.com>
2025-03-13 15:35:07 -0700

    partial findMinArrowShots

---
+++ 452 Minimum Number of Arrows to Burst Balloons.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        int findMinArrowShots(vector<vector<int>>& points) {
+            if (points.size() == 1) {
+                return 1;
+            }
+
+            for (int i = points.size() - 1;  i > 0; i--) {
+                
+            }
+            //combine by finding the max of the starts and the min of the ends
+        }
+    };
