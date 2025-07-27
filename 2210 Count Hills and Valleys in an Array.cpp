e15c383e7202209a1d9d969b5fb2935564106cca
scottmez <scottmesdjian10@gmail.com>
2025-07-26 21:57:43 -0700

    added daily problem 7/26

---
+++ 2210 Count Hills and Valleys in an Array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int countHillValley(vector<int>& nums) {
+        int count = 0;
+        int state = -1;
+        // 0 == hill
+        // 1 == valley
+        int prev = nums.at(0);
+        for (int num : nums) {
+            if (state == 0 && prev > num) {
+                count++;
+                state = 1;
+            }
+            else if (state == 1 && prev < num) {
+                count++;
+                state = 0;
+            }
+            else { //state == -1
+                if (prev > num) {
+                    state = 1;
+                }
+                else if (prev < num) {
+                    state = 0;
+                }
+            }
+            cout << state << endl;
+        }
+        return count;
+    }
+};
