853554862d0dc48239442c21c555b935d4f1ca1a
scottmez <scottmesdjian10@gmail.com>
2025-08-04 11:52:40 -0700

    improved the runtime of daily problem 8/4

--- 904 Fruit into Baskets.cpp
+++ 904 Fruit into Baskets.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        map<int, int> fruitIndices; // map to store the last index of each fruit
+        int lru_i = 0; // index of least recently used fruit
         int lru = fruits[0]; //least recently used fruit (second most recent)
-        int i = 1;
-        while (i < fruits.size() && fruits[i] == lru) {
-            i++; // skip all the same fruits at the start
+        int right = 1;
+        while (right < fruits.size() && fruits[right] == lru) {
+            right++; // skip all the same fruits at the start
         }
-        if (i == fruits.size()) {
-            return i; // if all fruits are the same, return the size of the array
+        if (right == fruits.size()) {
+            return right; // if all fruits are the same, return the size of the array
         }
-        int mru = fruits[i]; // most recently used fruit (most recent)
+        int mru = fruits[right]; // most recently used fruit (most recent)
+        int mru_i = right; // index of most recently used fruit
         int left = 0; // left pointer of the sliding window
-        int right = i; // right pointer of the sliding window
         int max = 2; // at least two fruits in the basket
-        fruitIndices[lru] = i-1; // store the index of the least recently used fruit
-        fruitIndices[mru] = i; // store the index of the most recently used fruit
         while (right < fruits.size()) {
             if (fruits[right] == lru) {
                 lru = mru; // update least recently used fruit
+                lru_i = mru_i; // update index of least recently used fruit
                 mru = fruits[right]; // update most recently used fruit
-                fruitIndices[mru] = right; // store the index of the most recently used fruit
+                mru_i = right; // update index of most recently used fruit
             }
             else if (fruits[right] != mru) {
                 max = std::max(max, right - left); // update max size of the basket
                 lru = mru; // update least recently used fruit
+                lru_i = mru_i; // update index of least recently used fruit
                 mru = fruits[right]; // update most recently used fruit
-                left = fruitIndices[lru]; // move left pointer to the next index after the last occurrence of lru
-                fruitIndices[mru] = right;
+                mru_i = right; // update index of most recently used fruit
+                left = lru_i; // move the left pointer to the index of least recently used fruit
             }
             right++; // move the right pointer to the next fruit
             //cout << "Left: " << left << ", Right: " << right << ", lru: " << lru << ", mru:" << mru << std::endl; // debug output
